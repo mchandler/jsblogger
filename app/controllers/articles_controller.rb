d835bf87730b383479d0f10dcc15492d501e69c2
Mike Chandler <mike@rmwebfx.com>
2012-02-03 15:34:49 -0800

    Complete update action.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,12 @@ class ArticlesController < ApplicationController
   
   def edit
     @article = Article.find(params[:id])
+  end
+  
+  def update
+    article = Article.find(params[:id])
+    article.update_attributes(params[:article])
     
+    redirect_to article_path(article)
   end
 end
