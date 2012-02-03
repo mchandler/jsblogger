9c481f20d03fe1e13f793a71627af6a8a10c7671
Mike Chandler <mike@rmwebfx.com>
2012-02-03 14:52:05 -0800

    New styles with simplified article save method.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    article = Article.new(:title => params[:article][:title], :body => params[:article][:body])
+    article = Article.new(params[:article])
     article.save
     
     redirect_to article_path(article)
