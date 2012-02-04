7b0288930500b25fdbc6c30b5cf35bfb3b4e5f74
Mike Chandler <mike@rmwebfx.com>
2012-02-03 16:54:46 -0800

    Handling validation on the update method.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,10 +26,13 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    article = Article.find(params[:id])
-    article.update_attributes(params[:article])
-    
-    redirect_to article_path(article)
+    @article = Article.find(params[:id])
+    if @article.update_attributes(params[:article])
+      redirect_to article_path(@article)
+    else
+      flash[:message] = "Hosed!!!"
+      render :show
+    end
   end
   
   def destroy
